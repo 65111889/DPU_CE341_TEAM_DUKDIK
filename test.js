e371b5f710994ceb8d083ecb2ec2f1dbd2160db3
65111889 <65111889@dpu.ac.th>
2023-11-17 02:08:12 +0700

    Commit

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
-// script.js
-// Function to get the IP address
 function getIPAddress() {
     fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('ip-address').innerText = 'IP ของคุณคือ : ' + data.ip;
+            document.getElementById('ip-address').innerText = 'IP Address ของคุณคือ : ' + data.ip;
         })
         .catch(error => console.error('Error fetching IP address:', error));
 }
-
-// Call the function when the page is loaded
 window.onload = getIPAddress;
